9565eec07537fdb680feaf61e9c03886b1f885dd
cdactest <cdaccybercloset@.com>
2020-11-07 13:13:12 +0530

    7 November

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,7 +1,7 @@
 class Constants {
   static int FLIPKART_BLUE = 0xff2874F0;
   static String POWERED_BY = "BRAIN MENTORS";
-  static String FLIPKART_SPLASH_LOGO = "assets/images/flipkart-logo-splash.png";
+  static String FLIPKART_SPLASH_LOGO = "assets/images/FLIPKART_SPLASH_LOGO.png";
   static String FLIPKART_LOGO = "assets/images/flipkart.jpg";
   static String HOME_ROUTE = "/home-screen";
   static String ROOT_ROUTE = "/";
